bb706e42ee2a37f369c57ba582d6f8d1de6adb53
taylor <taylor.viars@yahoo.com>
2022-11-13 18:39:15 -0500

    commit tree 6d9475c785865d106ef0bdb59e8994ab7604d00b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = (record.find(element => element.result === 'W'))
+     if(result) {
+         return(result.year)
+    }   else {return undefined}
+}
